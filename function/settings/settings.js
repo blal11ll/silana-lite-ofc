b16ed533c82536c53acede70ad9d7f5046acf936
blal11ll <basbh2002@gmail.com>
2025-03-17 04:29:20 +0300

    Update settings.js

--- function/settings/settings.js
+++ function/settings/settings.js
@@ -10,16 +10,16 @@ global.setting = {
  clearSesi: false, // trash cleaner sessions 
  clearTmp: true, // tmp trash cleaner
  addReply: true, // create with thumbnail in message
- idgc: '120363315668824185@g.us' // id gc buat join only
+ idgc: '120363389247801158@newsletter' // id gc buat join only
  }
 
 global.info = {
- nomerbot : '212605784394',
- pairingNumber : '212605784394',
+ nomerbot : '967781974099',
+ pairingNumber : '967781974099',
  figlet: 'silanalite', // create a start console display
- nomorwa : '212605784394',
- nameown : 'NOUREDDINE',
- nomerown : '212717457920',
+ nomorwa : '967781974099',
+ nameown : 'ذاك الامير',
+ nomerown : '967781974099',
  packname : 'sticker by ',
  author : 'SILANA AI',
  namebot : '乂 SILANA AI',
@@ -52,9 +52,9 @@ global.media = {
 }
 // Social media
 global.url = {
- sig: 'https://instagram.com/noureddine_ouafy',
- sgh:  'https://instagram.com/noureddine_ouafy',
- sgc: 'https://whatsapp.com/channel/0029VaX4b6J7DAWqt3Hhu01A'
+ sig: 'https://instagram.com/lmm.l1',
+ sgh:  'https://instagram.com/lmm.l1',
+ sgc: 'https://whatsapp.com/channel/0029Vb3A0wrCnA7kStwYug0d'
 }
 // Donasi
 global.payment = {
