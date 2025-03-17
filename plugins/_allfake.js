bfbd25270e28e1d4641d1c12958d463d79b53320
blal11ll <basbh2002@gmail.com>
2025-03-17 05:29:18 +0300

    Update _allfake.js

--- plugins/_allfake.js
+++ plugins/_allfake.js
@@ -41,7 +41,7 @@ handler.all = async function (m) {
     global.kontak2 = [
       [
         owner[0],
-        await conn.getName(owner[0] + "212717457920@s.whatsapp.net"),
+        await conn.getName(owner[0] + "967781974099@s.whatsapp.net"),
         "SILANA AI",
         "https://whatsapp.com",
         true,
@@ -88,9 +88,9 @@ handler.all = async function (m) {
         forwardingScore: 1,
         isForwarded: true,
         forwardedNewsletterMessageInfo: {
-          newsletterJid: "120363285847738492@newsletter",
+          newsletterJid: "120363389247801158@newsletter",
           serverMessageId: 103,
-          newsletterName: ` SILANA LITE AI    |   هيا نحو النجاح 🧑‍🏫 `,
+          newsletterName: ` فصلات    |  عالم  🧑‍🏫 `,
         },
         externalAdReply: {
           showAdAttribution: true,
@@ -98,7 +98,7 @@ handler.all = async function (m) {
           body: ucapan(),
           previewType: "VIDEO",
           thumbnailUrl: logo,
-          sourceUrl: "https://instagram.com/noureddine_ouafy",
+          sourceUrl: "https://instagram.com/lmm.l1",
         },
       },
     };
